3a7e09fca0b2c529c8027dc1086daf4eddbc664e
Anh Do <dinhanhdo95@gmail.com>
2021-06-21 21:15:29 +0700

    Update home's heading

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home-page">
     <div class="banner">
       <div class="container">
-        <h1 class="logo-font">CircleCI Configuration Tutorial</h1>
+        <h1 class="logo-font">CircleCI Latest Version</h1>
         <p>A place to share your knowledge.</p>
       </div>
     </div>
