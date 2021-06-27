f90e033df94d182dd7aa10b29fef139d928c1975
Anh Do <dinhanhdo95@gmail.com>
2021-06-27 11:44:37 +0700

    On hold

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home-page">
     <div class="banner">
       <div class="container">
-        <h1 class="logo-font">Another title</h1>
+        <h1 class="logo-font">On hold config</h1>
         <p>A place to share your knowledge.</p>
       </div>
     </div>
